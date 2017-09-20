bf304d2a9e4a0ef23f1c76a364a51c0c881c8c43
Matthew Wade <ozric@Matthews-MacBook-Pro.local>
2017-09-20 15:57:39 -0400

    update run script

--- DM5.Laureni/DM5_solver.m
+++ DM5.Laureni/DM5_solver.m
@@ -5,33 +5,33 @@
 init = [20,0,0,0,300,300]; %Initial conditions
 
 %Decay rates
-bA = 0;
-bN = 0;
+bA = 0.014;
+bN = 0.02;
 
 %Nitrogen incorporation
-iNa = 0;
-iNn = 0;
-iNx = 0;
+iNa = 0.083;
+iNn = 0.083;
+iNx = 0.058;
 
 %AMX concentration dependent on rAMX
-rAmx = 86; %mg/L/d
+rAmx = 86; %mgCOD/L/d
 muAmx = 0.017; %1/d
-XAmx = rAmx/muAmx; %mg/L
+XAmx = rAmx/muAmx; %mgCOD/L
 
 %DO concentration
-DO = 1.5;
+DO = 1.15; %mgO2/L
 
 %Biomass retention
-WAS = 5e-3;
+WAS = 4e-3;
 Bret = 1-WAS;
 
 params = [0.297,0.337,0.017,2.4,0.5,0.6,0.4,0.03,0.005,0.18,0.08,0.17,iNa,iNn,iNx,bA,bN,DO,XAmx]; %Growth parameters and fixed state values (O2 and AMX)
 tspan = [0,150];
 
-s1in = 20;
-s2in = 0;
-s3in = 0;
-s4in = 0;
+s1in = init(1);
+s2in = init(2);
+s3in = init(3);
+s4in = init(4);
 s1bar = 2;
 imp =@(s1) s1 > s1bar;
 xin = [s1in s2in s3in s4in];
@@ -55,12 +55,12 @@ legend({'AOB','NOB'})
 
 %Pre-impulse
 figure(3)
-plot(t0,ximp(:,1:4),'.','markersize',4)
+plot(t0,ximp(:,1:4),'-','markersize',4)
 xlabel('Time (d)')
 ylabel('Substrate concentrations (mgN L^{-1})')
 legend({'NH_4^+','NO_2^-','NO_3^-','N_2'})
 figure(4)
-plot(t0,ximp(:,5:6),'.','markersize',4)
+plot(t0,ximp(:,5:6),'-','markersize',4)
 xlabel('Time (d)')
 ylabel('Biomass concentrations (mgCOD L^{-1})')
 legend({'AOB','NOB'})
